c56c83b90c3aad86f074b912e4b3948f8ba75b67
Tetianadanyliv <tanyadanuliv@gmail.com>
2023-01-09 11:43:17 +0200

    HT-4

--- script/script.js
+++ script/script.js
@@ -3,15 +3,18 @@
 // // [...]];
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
-const studPairs = [];
-{
-  studPairs.push(
-    [students[0], students[2]],
-    [students[1], students[3]],
-    [students[4], students[5]]
-  );
+function getPairs(array = []) {
+  const studPairs = [];
+  {
+    studPairs.push(
+      [students[0], students[2]],
+      [students[1], students[3]],
+      [students[4], students[5]]
+    );
+    return studPairs;
+  }
 }
-console.log(studPairs);
+console.log(getPairs(students));
 
 // Зіставте пари з попереднього завдання та теми проєктів, над якими студенти
 // будуть працювати. Повинен вийти вкладений масив виду: [["Олександр і Олена",
@@ -22,15 +25,17 @@ const themes = [
   "Теорія автоматів",
   "Алгоритми та структури даних",
 ];
-const studProjects = [];
-{
-  studProjects.push(
-    [studPairs[0].concat(themes[0])],
-    [studPairs[1].concat(themes[1])],
-    [studPairs[2].concat(themes[2])]
-  );
-}
-console.log(studProjects);
+
+const studProjects = (stud, theme) => {
+  const newstudPairs = getPairs(stud.slice());
+  const themes = theme.slice();
+  let result = [];
+  for (let i = 0; i < newstudPairs.length; i++) {
+    result.push([newstudPairs[i].join(" i "), themes[i]]);
+  }
+  return result;
+};
+console.log(studProjects(students, themes));
 
 // Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]
 
@@ -52,8 +57,9 @@ function getRendomMark(studProjects) {
   let result = [];
   for (let i = 0; i < studProjects.length; i++) {
     const randomMark = (Math.random() * (5 - 1) + 1).toFixed(0);
-    result.push([...studProjects[i], randomMark]);
+    result.push([studProjects[i], randomMark]);
   }
   return result;
-}
-console.log(getRendomMark(studProjects));
+} 
+console.log(getRendomMark(studProjects(students, themes)));
+
